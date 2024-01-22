e0ed3c9e069dcd512400f80addb2988b1fafb15c
Mohammad Malek <mhos.malek@gmail.com>
2024-01-22 18:05:36 +0100

    update event to accept parameters for eamil

--- index.js
+++ index.js
@@ -23,34 +23,15 @@ exports.handler = (event, context, callback) => {
       },
     });
 
-  let _body = JSON.parse(event.body);
-
-  let body_txt = "<h2>Contact Form Details</h2>";
-  for (var key in _body) {
-    var res = key.replace("_", " ");
-    body_txt +=
-      "<p><strong>" + titleCase(res) + " : </strong>" + _body[key] + "</p>";
-  }
-
   let mailOptions = {
     from: process.env.SMTP_USERNAME,
     to: process.env.EMAIL_RECIPIENT,
     subject: "New contact form submission",
-    html: body_txt,
+    html: `<h2>New Contact Form Submission</h2>
+        <p><strong>Name:</strong> ${event.name}</p>
+        <p><strong>Email:</strong> ${event.email}</p>
+        <p><strong>Message:</strong><br>${event.message}</p>`,
   };
 
   transporter.sendMail(mailOptions, done(null, _body));
 };
-
-// convert the key as the label
-function titleCase(str) {
-  var splitStr = str.toLowerCase().split(" ");
-  for (var i = 0; i < splitStr.length; i++) {
-    // You do not need to check if i is larger than splitStr length, as your for does that for you
-    // Assign it back to the array
-    splitStr[i] =
-      splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
-  }
-  // return the joined string
-  return splitStr.join(" ");
-}
